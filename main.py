7e76ec6112edbd6b83f6550ef4375f1d5ac6d25c
Nick Sidelnikov <nikita04042000@gmail.com>
2022-11-14 15:56:29 +0300

    added args for features usage + report modification

--- main.py
+++ main.py
@@ -1,45 +1,90 @@
 import cv2
 import torch
+import argparse
+import time
+import psutil
+
+from pynvml.smi import nvidia_smi
 
 from my_utils import check_center, sqr_of_2_boxes, draw
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='weights/ppe_nano.pt', device=device)
+def main(input_video_path, perfomance_test, save_video):
+    if perfomance_test:
+        nvsmi = nvidia_smi.getInstance()
+        gpu_start = nvsmi.DeviceQuery('memory.free, memory.total')
+        ram_start = psutil.virtual_memory()[4]
 
-model.conf = 0.6
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model = torch.hub.load('ultralytics/yolov5', 'custom', path='weights/ppe_nano.pt', device=device)
+    model.conf = 0.6
 
-cap = cv2.VideoCapture('videos/test.mp4')
-ret = True
+    cap = cv2.VideoCapture(input_video_path)
+    if save_video:
+        fps = cap.get(cv2.CAP_PROP_FPS)
+        width = round(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = round(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+        out = cv2.VideoWriter('output.mp4', fourcc, fps, (width, height))
 
-while ret:
-    ret, frame = cap.read()
+    ret = True
+    start_time = time.time()
+    num_frames = 0
+    while ret:
+        ret, frame = cap.read()
+        num_frames += 1
+
+        if num_frames == 100:
+            nvsmi = nvidia_smi.getInstance()
+            gpu_loop = nvsmi.DeviceQuery('memory.free, memory.total')
+            ram_loop = psutil.virtual_memory()[4]
+        
+        pred = model(frame)
+        pred = pred.pandas().xyxy[0]
+
+        persons_data = pred.loc[pred['class'] == 0].copy()
+        persons_data["helm"] = None
+
+        ppe_data = pred.loc[pred['class'] != 0].copy()
+        ppe_data["used"] = None
+        
+        for index_person, row_person in persons_data.iterrows():
+            person_box = row_person.to_numpy()[:4].astype(int)
+            for index_ppe, row_ppe in ppe_data.iterrows():
+                ppe_box = row_ppe.to_numpy()[:4].astype(int)
+                if row_ppe['used'] == None:
+                    if (sqr_of_2_boxes(ppe_box, person_box) != 0) and (check_center(ppe_box, person_box)):
+                        persons_data['helm'] = index_ppe
+                        row_ppe['used'] = True
+
+        if not persons_data.empty:
+            draw_frame = draw(persons_data, ppe_data, frame)
+            cv2.imshow('test', draw_frame)
+            if save_video:
+                out.write(draw_frame)
+        else:
+            cv2.imshow('test', frame)
+            if save_video:
+                out.write(draw_frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
     
-    pred = model(frame)
-    pred = pred.pandas().xyxy[0]
+    loop_time = time.time()
 
-    persons_data = pred.loc[pred['class'] == 0].copy()
-    persons_data["helm"] = None
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
 
-    ppe_data = pred.loc[pred['class'] != 0].copy()
-    ppe_data["used"] = None
-    
-    for index_person, row_person in persons_data.iterrows():
-        person_box = row_person.to_numpy()[:4].astype(int)
-        for index_ppe, row_ppe in ppe_data.iterrows():
-            ppe_box = row_ppe.to_numpy()[:4].astype(int)
-            if row_ppe['used'] == None:
-                if (sqr_of_2_boxes(ppe_box, person_box) != 0) and (check_center(ppe_box, person_box)):
-                    persons_data['helm'] = index_ppe
-                    row_ppe['used'] = True
+    if perfomance_test:
+        fps = num_frames / loop_time
+        gpu_memory_usage = round(float(gpu_start['gpu'][0]['fb_memory_usage']['free']) - float(gpu_loop['gpu'][0]['fb_memory_usage']['free']), 3)
+        ram_usage = round((ram_start - ram_loop)/1024/1024, 3)
+        print("fps: {} || ram_usage: {} || gpu_memory_usage: {} MiB".format(round(fps, 3), ram_usage, gpu_memory_usage))
 
-    if not persons_data.empty:
-        draw_frame = draw(persons_data, ppe_data, frame)
-        cv2.imshow('test', draw_frame)
-    else:
-        cv2.imshow('test', frame)
+parser = argparse.ArgumentParser()
+parser.add_argument('--video_path', default='videos/test.mp4')
+parser.add_argument('--save_video', default=False)
+parser.add_argument('--perfomance_test', default=False)
+args = parser.parse_args()
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-cap.release()
-cv2.destroyAllWindows()
+main(args.video_path, args.perfomance_test, args.save_video)
